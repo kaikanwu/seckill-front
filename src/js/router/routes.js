3f6fd4e065fe6608f8dbad01c677b21bfc03301a
kaikanwu <wukaikan@gmail.com>
2018-09-19 23:38:26 +0800

    :zap: course list function

--- src/js/router/routes.js
+++ src/js/router/routes.js
@@ -2,11 +2,25 @@ import Login from '../../components/Login'
 import Foo from '../../components/Foo'
 import Bar from '../../components/Bar'
 import Register from '../../components/Register'
+import HomeTemplate from '../../components/HomeTemplate'
+import CourseList from '../../components/CourseList'
+// import OrderList from '../../components/OrderList'
+import Course from '../../components/Course'
+import OrderList from '../../components/CourseList'
+
 export default[
     {path:'/',component: Login},
     {path:'/login',component:Login},
     {path:'/foo', component:Foo},
     {path:'/bar', component:Bar},
-    {path: '/register', component:Register}
+    {path: '/register', component:Register},
+    { path: '/home', component: HomeTemplate,
+    children: [
+      { path: 'course/list', component: CourseList },
+      {path: 'order/list', component: OrderList},
+    //   { path: 'order/list', component: OrderList },
+      { path: 'course/:courseNo', component: Course }
+    ]
+  },
 
 ]
